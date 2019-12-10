126119cf1990e7edb8e86b7e76e81447e4956786
Hamzah Chaudhry <haz540@gmail.com>
2019-12-10 03:30:03 -0500

    Added scripts and functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
+// import needed sdks
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// init app
+admin.initializeApp();
+const db = admin.firestore();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+exports.helloWorld = functions.https.onRequest((req, res) => {
+    res.send("Hello from Firebase!");
+});
+
+// get nba teams
+exports.teamsNba = functions.https.onRequest(async (req, res) => {
+    const snapshot = await db.collection('nba').get();
+    res.send(snapshot.docs.map(doc => doc.data()));
+});
